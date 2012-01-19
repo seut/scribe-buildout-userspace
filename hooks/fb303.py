cdc6658ecbca8cf63b7890f7b4dce146de5b5d94
andi <andi@vz.net>
2012-01-19 17:14:19 +0100

    - build further (fb303 path issues seems done). ! there is a compiler issue now

--- hooks/fb303.py
+++ hooks/fb303.py
@@ -7,24 +7,26 @@ log = logging.getLogger('fb303-hook')
 def premake(options, buildout):
     # import pdb; pdb.set_trace()
     basedir = options['path']
-    makefile = os.path.join(basedir, "Makefile")
+    makefile_list = [os.path.join(basedir, "Makefile"), 
+            os.path.join(basedir, "cpp", "Makefile")]
 
     log.debug('pre-make-hook on %s' % basedir)
 
-    log.info("fixing path in ``%s``" % makefile)
-    
-    try:
-        os.remove(makefile+"~")
-    except:
-        pass;
-    os.rename(makefile, makefile+"~")
-    dst = open(makefile, "w")
-    src = open(makefile+"~", "r")
-    
-    for line in src:
-        if "prefix = /usr/local" in line:
-            log.debug("... %s" % line)
-            line=line.replace('/usr/local', buildout['thrift']['location'])
-            log.info(">>> %s" % line)
-        dst.write(line)
-    fileinput.close()
+    for makefile in makefile_list:
+        log.info("fixing path in ``%s``" % makefile)
+        
+        try:
+            os.remove(makefile+"~")
+        except:
+            pass;
+        os.rename(makefile, makefile+"~")
+        dst = open(makefile, "w")
+        src = open(makefile+"~", "r")
+        
+        for line in src:
+            if "prefix = /usr/local" in line:
+                log.debug("... %s" % line)
+                line=line.replace('/usr/local', buildout['thrift']['location'])
+                log.info(">>> %s" % line)
+            dst.write(line)
+        fileinput.close()
